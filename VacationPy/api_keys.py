0f7cfb07f264c6fc28963a7d825ea9b1f8b22f1a
diazdez <69435783+diazdez@users.noreply.github.com>
2020-10-11 13:12:57 -0500

    updated hw

---
+++ VacationPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "9a1cd8f986a805de013d19b41d9ef01f"
+
+# Google API Key
+g_key = "AIzaSyCgLVl_oQ_1TW7eQurySpiLVeIMN99g35o"
---
+++ api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "top secret
+
+# Google API Key
+g_key = "top secret"
