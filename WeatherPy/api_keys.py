1867ac4843ccf605cd54f4914990bbaf9d8b2524
diazdez <69435783+diazdez@users.noreply.github.com>
2020-10-11 13:16:45 -0500

    updated hw

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "9a1cd8f986a805de013d19b41d9ef01f"
+weather_api_key = "top secret
 
 # Google API Key
-g_key = "AIzaSyCgLVl_oQ_1TW7eQurySpiLVeIMN99g35o"
+g_key = "top secret"
